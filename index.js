636e72c5a012d05ea2b0ccc0eba389639ad3705f
robertay99 <robertay99@hotmail.com>
2024-09-23 08:01:06 -0300

    updated index.js file

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Get the input element by its ID
+    const input = document.getElementById('button');
+  
+    // Define the clickAlert function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener for the 'click' event
+    input.addEventListener('click', clickAlert);
+  }
+  
